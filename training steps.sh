3e900afb876495a3acffad85cfd594bd10f093fb
pddenhar@gmail.com <pddenhar@gmail.com>
2017-04-11 17:15:46 -0500

    Found optimal number of stages to be ~22, training to AR of 1.0e-5

--- training steps.sh
+++ training steps.sh
@@ -4,4 +4,4 @@ opencv_annotation --annotations=annotations.txt --images=positives/
 
 opencv_createsamples -info annotations.txt -bg negatives.txt -vec positives.txt -w 24 -h 24  
 
-opencv_traincascade -data cascade_dir -vec positives.txt -bg negatives.txt -numPos 430 -numNeg 450 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024
+opencv_traincascade -data cascade_dir -vec positives.txt -bg negatives.txt -numPos 430 -numNeg 450 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 25 -acceptanceRatioBreakValue 1.0e-5
