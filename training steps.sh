ca80595a272f88a5232a02d23dddfd21032fee25
pddenhar@gmail.com <pddenhar@gmail.com>
2017-04-11 16:47:41 -0500

    Add images, mildly trained classifier and a lot of annotations

---
+++ training steps.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+opencv_annotation --annotations=annotations.txt --images=positives/ 
+
+opencv_createsamples -info annotations.txt -bg negatives.txt -vec positives.txt -w 24 -h 24  
+
+opencv_traincascade -data cascade_dir -vec positives.txt -bg negatives.txt -numPos 114 -numNeg 450 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024
